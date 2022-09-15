aa61b11a341be8eddfd8c625a955ad164f78dd48
Om Patil <omajitsinhpatil@gmail.com>
2022-09-15 22:13:49 +0530

    third

--- Desktop/carousel backup/final/carousel/om.java
+++
@@ -1,8 +0,0 @@
-public class  Solution{
-    public static void main(String [] args)  {
-        double a = 6 / 4;
-        int b  = 6 / 4;
-        double c = a + b;
-        System.out.println(c);
-    }
-}
